8dbd0bd1730e602f639ceddc4dcef0ae5d7444bd
郭斌勇 <guobinyong@qq.com>
2022-05-26 14:34:28 +0800

    chore: 优化构建配置

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@
 import { defineConfig } from 'vite'
 import {getDependencieNames,getBaseNameOfHumpFormat,removeScope} from "package-tls";
 import pkg from "./package.json";
-import {dirname} from "path"
+import {dirname} from "path";
+import {build} from "vite";
 
 
 // 手动配置
 const entry = 'src/index.ts';   // 输入（入口）文件
-const formats = ['es', 'umd'];  //所需构建的模块格式
+//所需构建的模块格式
+const formatsForExcludeDep = ['es', 'umd'];  //要排除依赖包的模块格式
+const formatsForIncludeDep = ['iife'];  //要包含依赖包的模块格式
+
 
 // 自动配置
 const pkgName = getBaseNameOfHumpFormat(pkg.name);  //驼峰格式的 pkg.name
@@ -24,7 +28,6 @@ const config = {
         lib: {
             name:pkgName, 
             entry: entry,
-            formats:formats,
         },
         outDir:outDir,
         rollupOptions:{
@@ -40,16 +43,22 @@ const config = {
 export default defineConfig((options)=>{
     const {mode} = options;
 
+
     switch (mode) {
         case "stage":{
+            config.build.lib.formats = [...formatsForExcludeDep,...formatsForIncludeDep];
             config.build.rollupOptions.external = getDependencieNames(pkg,["peerDependencies"]);
             break;
         }
-        case "iife":{
-            config.build.emptyOutDir = false;
-            config.build.lib.formats = ["iife"];
-            config.build.rollupOptions.external = getDependencieNames(pkg,["peerDependencies"]);
-            break;
+        default: {
+            if (formatsForIncludeDep.length>0){
+                const inlineConfig = JSON.parse(JSON.stringify(config));
+                inlineConfig.configFile = false; // 防止死循环：循环调用此函数
+                inlineConfig.build.emptyOutDir = false; // 不清空输出目录
+                inlineConfig.build.lib.formats = formatsForIncludeDep;
+                inlineConfig.build.rollupOptions.external = getDependencieNames(pkg,["peerDependencies"]);
+                build(inlineConfig); //单独进行构建
+            }
         }
     }
 
